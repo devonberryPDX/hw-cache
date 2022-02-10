bd606312c7b74145df3cec02d12e9afd35cc937b
devonberry <67202514+devonberry@users.noreply.github.com>
2022-02-10 09:31:14 -0800

    Believe to have made default impl

--- null/src/lib.rs
+++ null/src/lib.rs
@@ -11,11 +11,11 @@ use std::marker::PhantomData;
 
 /// A "cache" that does not cache anything. Useful
 /// for testing behavior with cache misses.
-pub struct NullCache <I>();
+pub struct NullCache <I>(PhantomData<I>);
 
 impl<I> Default for NullCache<I> {
     fn default() -> Self {
-        todo!()
+        NullCache {0: PhantomData}
     }
 }
 
---
+++ target/debug/deps/lru-9e33d3cfeca99990.d
@@ -0,0 +1,7 @@
+D:\CS Files\PSU CS Files\winterTerm2022\rust\hw-cache\target\debug\deps\lru-9e33d3cfeca99990.rmeta: lru\src\lib.rs
+
+D:\CS Files\PSU CS Files\winterTerm2022\rust\hw-cache\target\debug\deps\lru-9e33d3cfeca99990.d: lru\src\lib.rs
+
+lru\src\lib.rs:
+
+# env-dep:CLIPPY_ARGS=
